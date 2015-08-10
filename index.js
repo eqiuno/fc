ab6edbf1f7b9642338f718d144afe5d432db4d58
liupeng <ian.liu@runmit.com>
2015-08-10 16:08:16 +0800

    add more test

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
-var bb = require('bluebird');
 var slice = Array.prototype.slice;
 
-module.exports = exports = function(fns) {
-	if(!Array.isArray(fns) || fns.length === 0) {
-		throw new Error('required function array is missed');
-	}
+module.exports = exports = function (fns) {
+    if (!Array.isArray(fns)) {
+        fns = slice.call(arguments);
+    }
 
+    return new Promise(function (resolve, reject) {
+        var idx = 0;
 
-	return new Promise(function(resolv, reject) {
-		var idx = 0;	
+        function onFilled(data) {
+            if (idx < fns.length) {
+                next(data);
+                idx++;
+            } else {
+                resolve(data);
+            }
+        }
 
-		function onFilled(data) {
-			if(idx < fns.length) {
-				next(data);
-				idx++;
-			} else {
-				resolv(data);
-			}
-		}
+        function onReject(err) {
+            reject(err);
+        }
 
-		function onReject(err) {
-			reject(err);
-		}
+        function next(val) {
+            var fn = fns[idx];
+            var rtn;
+            if (isPromise(fn)) {
+                rtn = fn;
+            }
+            else if (isFunction(fn)) {
+                try {
+                    rtn = fn.apply(this, slice.call(arguments));
+                } catch (e) {
+                    return reject(e);
+                }
+            }
 
-		function next(val) {
-			var fn = fns[idx];
-			var rtn = fn.apply(this, val);
-			if(!isPromise(rtn)) {
-				throw new Error('fn is not promise');
-			}
+            if (!isPromise(rtn)) {
+                var p = new Promise(function (resolve) {
+                    resolve(rtn);
+                });
+                rtn   = p;
+            }
+            return rtn.then(onFilled, onReject);
+        }
 
-			rtn.then(onFilled, onReject);
-		}
-	});
-	
-	function isPromise(rtn) {
-		return 'function' === typeof rtn.then;
-	}
-}
+        next();
+    });
+
+    function isPromise(rtn) {
+        return rtn && 'function' === typeof rtn.then;
+    }
+
+    function isFunction(obj) {
+        return typeof obj === 'function';
+    }
+};
--- test/test.js
+++ test/test.js
@@ -1,17 +1,108 @@
-
 var assert = require('assert');
-var bb = require('bluebird');
-var steps = require('promise-steps');
-describe('steps', function(done) {
-	it('array', function(done) {
-		var a = bb.resolve(1);
-		var b = bb.resolve(2);
-		var c = bb.resolve(3);
-		var rtn = steps([a, b, c]);
+var bb     = require('bluebird');
+var fc     = require('..');
+describe('fc_promises', function () {
+    it('array', function (done) {
+        var a   = bb.resolve(1);
+        var b   = bb.resolve(2);
+        var c   = bb.resolve(3);
+        var rtn = fc([a, b, c]);
 
-		rtn.then(function(val) {
-			assert(3, val);
-			done();
-		});
-	});
-})
+        rtn.then(function (val) {
+            assert(3, val);
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    });
+});
+
+describe('fc_functions', function () {
+    it('array', function (done) {
+        var rtn = fc([f(1), f(2), f(3)]);
+
+        rtn.then(function (val) {
+            assert(3, val);
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    });
+
+    it('throw', function (done) {
+        var rtn = fc([f(1), function () {
+            throw new Error('abc');
+        }, f(3)]);
+        rtn.then(function () {
+            done(new Error('error is not thrown'));
+        }).catch(function (err) {
+            assert(err.message, 'test');
+            done();
+        })
+    })
+});
+
+describe('fc_arguments', function () {
+    it('arguments', function (done) {
+        var a   = bb.resolve(1);
+        var b   = bb.resolve(2);
+        var c   = bb.resolve(3);
+        var rtn = fc(a, b, c);
+        rtn.then(function (v) {
+            assert(v, 3);
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+    })
+});
+
+describe('fc_pass_value', function () {
+    it('val_pass', function (done) {
+        function a() {
+            return 1;
+        }
+
+        function b(v) {
+            return 2 + v;
+        }
+
+        function c(v) {
+            return 3 + v;
+        }
+
+        var rtn = fc(a, b, c).then(function (v) {
+            assert(v, 6);
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+    });
+    it('promise_pass', function (done) {
+        function a() {
+            return Promise.resolve(1);
+        }
+
+        function b(v) {
+            return Promise.resolve(2 + v);
+        }
+
+        function c(v) {
+            return Promise.resolve(3 + v);
+        }
+
+        var rtn = fc(a, b, c).then(function (v) {
+            assert(v, 6);
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+    });
+});
+
+
+function f(n) {
+    return function () {
+        return bb.resolve(n);
+    }
+}
